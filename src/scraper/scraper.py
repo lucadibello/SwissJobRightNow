ec87186f8d33201050a739789ac34e01258c2a02
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-17 16:31:56 +0100

    Changed scraper path

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -122,6 +122,6 @@ print(" - Phone numbers not found:", telErrorConter)
 print("")
 
 print("💆 Exporting research data...")
-with open('.temp/jobs.json', 'w+') as f:
+with open('src/scraper/.temp/jobs.json', 'w+') as f:
     f.write(json.dumps(workDicts,indent=4))
 print("✅ Data exported successfully in 'output.json'")
