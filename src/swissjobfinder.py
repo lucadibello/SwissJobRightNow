fe11e829dd3f246b3abe118cbba0904425c40454
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-18 11:06:06 +0100

    Now conversion server is started automatically + generation

--- src/swissjobfinder.py
+++ src/swissjobfinder.py
@@ -1,28 +1,46 @@
 import subprocess
-import threading
+from sys import stderr
 import time
 from modules import Config
 from modules import Scraper as JobScraper
 from modules import Generator as PresentationLetterGenerator
 
-
-MAX_PAGES = 1
 def main ():
-
+  # Load config file
   config = Config("config/config.json")
 
+  # Start conversion server
+  conversionServer = subprocess.Popen(
+    args=["yarn", "--cwd", "/Users/lucadibello/Documents/Repo/SwissJobRightNow/src/conversion-server", "start"],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    shell=False
+  )
+   
   # Init job scraper
   jobs = JobScraper(config)
   # Scrape + get a scrape report
-  report = jobs.scrape(MAX_PAGES)
-  # Print report
-  print(report.jobs)
-  
-  # Start conversion server
-  # asd = subprocess.Popen(args=["yarn", "--cwd", "src/conversion-server", "start"], stdout=subprocess.PIPE)
+  report = jobs.scrape()
 
+  # Wait for conversion server to setup
+  print("🔎 Waiting for conversion server to setup..")
+  for line in conversionServer.stdout:
+    # Read output
+    output = line.decode().replace("\n", "")
+
+    # Check if server is ready      
+    if output == "CONVERSION_SERVER_READY":
+      # Server is ready!
+      print("✅ Conversion server is ready..")
+      break
+  
   # Start generator
-  letterGenerator = PresentationLetterGenerator()
+  print("⚠️ Starting presentation letter generation...")
+  letterGenerator = PresentationLetterGenerator(config, report)
+  letterGenerator.generateAll()
+
+  # Kill conversion server before exiting
+  conversionServer.kill()
 
 if __name__ == '__main__':
   main()
