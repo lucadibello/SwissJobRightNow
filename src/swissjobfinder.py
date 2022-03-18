dd6fb5747664e51f49395bafcbf363f82d1cb56c
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-18 11:12:17 +0100

    Fixed #2

--- src/swissjobfinder.py
+++ src/swissjobfinder.py
@@ -8,14 +8,6 @@ from modules import Generator as PresentationLetterGenerator
 def main ():
   # Load config file
   config = Config("config/config.json")
-
-  # Start conversion server
-  conversionServer = subprocess.Popen(
-    args=["yarn", "--cwd", "/Users/lucadibello/Documents/Repo/SwissJobRightNow/src/conversion-server", "start"],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    shell=False
-  )
    
   # Init job scraper
   jobs = JobScraper(config)
@@ -24,23 +16,31 @@ def main ():
 
   # Wait for conversion server to setup
   print("🔎 Waiting for conversion server to setup..")
-  for line in conversionServer.stdout:
-    # Read output
-    output = line.decode().replace("\n", "")
 
-    # Check if server is ready      
-    if output == "CONVERSION_SERVER_READY":
-      # Server is ready!
-      print("✅ Conversion server is ready..")
-      break
+  # Start conversion server
+  with subprocess.Popen(
+    args=["yarn", "--cwd", "/Users/lucadibello/Documents/Repo/SwissJobRightNow/src/conversion-server", "start"],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    shell=False
+  ) as conversionServer:
+
+    for line in conversionServer.stdout:
+      # Read output
+      output = line.decode().replace("\n", "")
+
+      # Check if server is ready      
+      if output == "CONVERSION_SERVER_READY":
+        # Server is ready!
+        print("✅ Conversion server is ready..")
+        break
   
-  # Start generator
-  print("⚠️ Starting presentation letter generation...")
-  letterGenerator = PresentationLetterGenerator(config, report)
-  letterGenerator.generateAll()
+    # Start generator
+    print("⚠️ Starting presentation letter generation...")
+    letterGenerator = PresentationLetterGenerator(config, report)
+    letterGenerator.generateAll()
 
-  # Kill conversion server before exiting
-  conversionServer.kill()
-
+    # Kill server
+    conversionServer.kill()
 if __name__ == '__main__':
   main()
