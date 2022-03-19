4afbb14c15814eaad0d97873c4406624632d73d6
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-19 14:11:09 +0100

    Query builder now accepts region or canton flags

--- src/modules/generator/queries/querybuilder.py
+++ src/modules/generator/queries/querybuilder.py
@@ -4,9 +4,15 @@ class QueryBuilder:
   __LOCAL_BASE_QUERY_STRING = "https://www.local.ch/it/q"
   
   @classmethod
-  def build_page_query_link (cls, region: str, jobKind: str, onlyCompanies: bool = True) -> str:
+  def build_page_query_link (cls, area: str, jobKind: str, isAreaARegion: bool, isAreaACanton: bool, onlyCompanies: bool = True) -> str:
+    # Build area string
+    if (isAreaARegion):
+      area += " (Regione)"
+    elif (isAreaACanton):
+      area += " (Canton)"
+
     # Build URL
-    baseString = cls.__LOCAL_BASE_QUERY_STRING + "/" + parser.quote(region) + "/" + parser.quote(jobKind)
+    baseString = cls.__LOCAL_BASE_QUERY_STRING + "/" + parser.quote(area) + "/" + parser.quote(jobKind)
  
     if onlyCompanies:
       # Add filter for companies
