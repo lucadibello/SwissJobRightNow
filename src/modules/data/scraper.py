0cac9c15c5dd2cdae149ab76eba8be77521d7455
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-19 15:40:11 +0100

    Huge bug solved

--- src/modules/data/scraper.py
+++ src/modules/data/scraper.py
@@ -9,10 +9,10 @@ class ScraperReport:
   nJobsWithoutEmail = 0
 
   def increaseJobsWithoutNumber(self):
-    ++self.nJobsWithoutNumber
+    self.nJobsWithoutNumber += 1
   
   def increaseJobsWithoutEmail(self):
-    ++self.nJobsWithoutEmail
+    self.nJobsWithoutEmail += 1
 
 class Scraper:
   # Error flags
@@ -43,95 +43,95 @@ class Scraper:
 
       # Read page data
       print("📚 Page n.", nPage, "/", MAX_PAGES)
-      r = requests.get(self.dataUrl)
+      r = requests.get(self.dataUrl + str(nPage))
 
       # Check page response
       if (r.status_code == 404):
         # Throw exception
         raise Exception("❌ Cannot perform request. Status code 404")
   
-    # Create scraper object
-    soup = BeautifulSoup(r.text, "html.parser")
+      # Create scraper object
+      soup = BeautifulSoup(r.text, "html.parser")
 
-    # Read all information from page
-    for idx,item in enumerate(soup.find_all("div", class_="js-entry-card-container")):
-      workJson = item.find("a", class_="card-info").get("data-gtm-json")
-      contactsJson = item.find("div", class_="entry-actions")
+      # Read all information from page
+      for idx, item in enumerate(soup.find_all("div", class_="js-entry-card-container")):
+        workJson = item.find("a", class_="card-info").get("data-gtm-json")
+        contactsJson = item.find("div", class_="entry-actions")
 
-      tel = ""
-      mail = ""
-      if contactsJson is not None:
-        # Read phone number
-        tel = contactsJson.find("span",class_="visible-print action-button text-center")
-        # Read email
-        mail = contactsJson.find("a", class_="js-gtm-event js-kpi-event action-button text-center hidden-xs hidden-sm action-button-default")
+        tel = ""
+        mail = ""
+        if contactsJson is not None:
+          # Read phone number
+          tel = contactsJson.find("span",class_="visible-print action-button text-center")
+          # Read email
+          mail = contactsJson.find("a", class_="js-gtm-event js-kpi-event action-button text-center hidden-xs hidden-sm action-button-default")
 
-        # Check if present
-        if mail is not None:
-          # Phone found
-          mail = mail.get("href")[7::]
-          print("✅ Email found:", mail)
+          # Check if present
+          if mail is not None:
+            # Phone found
+            mail = mail.get("href")[7::]
+            print("✅ Email found:", mail)
+          else:
+            # Email not found
+            print("❌  Email not found")
+            self.report.increaseJobsWithoutEmail()
+
+          # Check if phone number is present
+          if tel is not None:
+            # Phone found
+            tel = tel.text.strip()
+            print("✅  Phone number found", tel)
+          else:
+            # Phone not found
+            print("❌  Phone number not found")
+            self.report.increaseJobsWithoutNumber()
         else:
-          # Email not found
-          print("❌  Email not found")
+          # Email and phone not found!
+          print("❌ Contacts (Email or Phone) not found, skip this job")
           self.report.increaseJobsWithoutEmail()
-
-        # Check if phone number is present
-        if tel is not None:
-          # Phone found
-          tel = tel.text.strip()
-          print("✅  Phone number found", tel)
-        else:
-          # Phone not found
-          print("❌  Phone number not found")
           self.report.increaseJobsWithoutNumber()
-      else:
-        # Email and phone not found!
-        print("❌ Contacts (Email or Phone) not found, skip this job")
-        self.report.increaseJobsWithoutEmail()
-        self.report.increaseJobsWithoutNumber()
 
-      full = item.find("div", class_="card-info-address").find("span").text.split(", ")
+        full = item.find("div", class_="card-info-address").find("span").text.split(", ")
 
-      # Read house number
-      try:
-        houseNumber = full[0].split()[-1]
-      except:
-        houseNumber = ""
+        # Read house number
+        try:
+          houseNumber = full[0].split()[-1]
+        except:
+          houseNumber = ""
 
-      # Read CAP
-      try:
-        cap = full[1].split()[0]
-      except:
-        cap = ""
-      
-      # Read address
-      try:
-        data = full[0].split()
-        houseNumberIndex = len(data) - 1
-        address = ' '.join(data[0:houseNumberIndex])
-      except:
-        address = ""
+        # Read CAP
+        try:
+          cap = full[1].split()[0]
+        except:
+          cap = ""
+        
+        # Read address
+        try:
+          data = full[0].split()
+          houseNumberIndex = len(data) - 1
+          address = ' '.join(data[0:houseNumberIndex])
+        except:
+          address = ""
 
-      # Convert to dict
-      workDict = json.loads(workJson)
+        # Convert to dict
+        workDict = json.loads(workJson)
 
-      # Notify user
-      print("😎 Found new possible job:", workDict.get("DetailEntryName"))
-      print("")
+        # Notify user
+        print("😎 Found new possible job:", workDict.get("DetailEntryName"))
+        print("")
 
-      # Append data to dict
-      workDicts.append({
-        'nome': workDict.get("DetailEntryName"),
-        'paese': workDict.get("DetailEntryCity"),
-        'cap': cap,
-        'address': address,
-        'houseNumber': houseNumber,
-        'email': mail,
-        'telefono': tel,
-        'page': (nPage-1),
-        'link': 'https://www.local.ch/it/q/Sottoceneri%20(Regione)/informatica?page=' + str(nPage-1)
-      })
+        # Append data to dict
+        workDicts.append({
+          'nome': workDict.get("DetailEntryName"),
+          'paese': workDict.get("DetailEntryCity"),
+          'cap': cap,
+          'address': address,
+          'houseNumber': houseNumber,
+          'email': mail,
+          'telefono': tel,
+          'page': (nPage-1),
+          'link': 'https://www.local.ch/it/q/Sottoceneri%20(Regione)/informatica?page=' + str(nPage-1)
+        })
 
     # Save data inside report
     self.report.jobs = workDicts
