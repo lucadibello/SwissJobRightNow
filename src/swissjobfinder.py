252cabc3ae0c9eea077cb781a2abdfc7a6663008
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-03-19 15:21:47 +0100

    Now show report while executing

--- src/swissjobfinder.py
+++ src/swissjobfinder.py
@@ -15,7 +15,7 @@ def main ():
     email with the cover letter and other custom attachments
     (specified via config.json) to the company automatically.
     Author: Luca Di Bello - @lucadibello.ch""")
-    
+
   parser.add_argument("area", metavar="JOB_AREA", type=str, help="Set an area (City, Region or Canton) where you want to work. Specify the kind of area with related flags. By default Area is a city.")
   parser.add_argument("kind", metavar="JOB_KIND", nargs='+',type=str, help="Set the kind of job desired (ex: Computer Science)")
   parser.add_argument("-A", action="store_true", help="Send E-Mail to any kind of contact (companies and privates). By default the application are sent only to company contacts")
@@ -40,6 +40,14 @@ def main ():
   
   # Scrape + get a scrape report
   report = jobs.scrape()
+
+  # Print report
+  print("📊 SwissJobRightNow - Finder report")
+  print(" - Total of contacts found:", len(report.jobs))
+  print(" - Total of contacts without E-Mail:", report.nJobsWithoutEmail)
+  print(" - Total of contacts without phone number:", report.nJobsWithoutNumber)
+
+  input("Press enter to continue with presentation letter generation...")
 
   # Wait for conversion server to setup
   print("🔎 Waiting for conversion server to setup..")
@@ -74,7 +82,13 @@ def main ():
   if input('Everything is ready now. Would you like to proceed with submitting your applications? (y/n) ') == 'y':
     # Send mail
     mailSender = MailSender(config, report)
-    mailSender.sendAll()
+    sentReport = mailSender.sendAll()
+
+    # Print report
+    print("📊 SwissJobRightNow - Mailer report")
+    print(" - Total possible applications:", len(report.jobs))
+    print(" - Total sent application:", sentReport.emailSentCounter)
+    print(" - Skipped contacts (errors):", sentReport.emailErrorCounter)
   else:
     print("Operation aborted. Any application has been sent")
 
